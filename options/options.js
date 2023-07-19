ed800969fcfb5ec00a3c1bb82223bb9573440e06
Crowdbotics <team@crowdbotics.com>
2023-07-19 17:25:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "perforation-42281",
+  url: "https://perforation-42281.botics.co",
+  api: "https://perforation-42281.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
